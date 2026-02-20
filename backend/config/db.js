fd4de69f764b3d5865b180d8b26bc9c883c0a365
Mohit Monnappa <mohitmonnappa@gmail.com>
2026-02-20 08:58:59 +0530

    implemented event logic, updated database connections

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,15 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: false, // This forces Node to accept the Aiven certificate
   },
 });
 
 pool.connect()
   .then(() => console.log("PostgreSQL connected successfully 🚀"))
-  .catch(err => console.error("Database connection error", err));
+  .catch(err => console.error("Database connection error:", err.message));
 
 module.exports = pool;
